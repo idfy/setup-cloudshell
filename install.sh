abf0563a4270847a0cb075ccabf299b8b3f8b1f8
Kaushik Shah <kaushik@idfy.com>
2023-03-23 19:56:31 +0530

    fixes

--- install.sh
+++ install.sh
@@ -1,3 +1,5 @@
+rm -rf ~/setup-cloudshell
 git clone https://github.com/idfy/setup-cloudshell ~/setup-cloudshell
 cd ~/setup-cloudshell
+chmod +x ~/setup_cloudshell_ssh_config.sh
 /bin/bash -c setup_cloudshell_ssh_config.sh
